cec91c38c0d4caca5afd21e6a96ac55e26ba0d8f
Alex Costa & Jeff Austin <student@epicodus.com>
2015-09-10 15:11:59 -0700

    Make finding roman numerals magical, majestic, a blessing of unicorns descended upon us when we finished this. With every keystroke a fairy was born.

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,44 @@
-
-
 function romanConvert(num) {
 
-    var romanArray =[];
 
-    //if number is greater then 3999 roman numerals cant get that high bozo
-    if(num > 3999) {
-        return "Roman numerals can not go higher then 3,999 :(";
-    }
+    var numeralList = [
+        [1 , "I"],
+        [4 , "IV"],
+        [5 , "V"],
+        [9 , "IX"],
+        [10 , "X"],
+        [40 , "XL"],
+        [50 , "L"],
+        [90 , "XC"],
+        [100 , "C"],
+        [400 , "CD"],
+        [500 , "D"],
+        [900 , "CM"],
+        [1000 , "M"]
+    ];
 
-    //take num convert to array
-    var numArray = (""+num).split("");
+    var romanArray = [];
 
-    //flip array so we can track number place
-    var flipArray = numArray.reverse();
+    for(var i = 12; i >= 0; i--){
 
-    //set 0 to ones 1 to tens 3 to hundreds and 4 to thousands
+        var flooredNum = Math.floor(num/numeralList[i][0]) * numeralList[i][0];
+        console.log(flooredNum);
 
-    /*********** Ones Place ********************************/
+        if(num >= flooredNum)
+        {
 
-    //Special conditions for 4, 5 and 9
-    if(flipArray[0] === '4') {
-        romanArray.unshift('IV');
-    }
+            if(numeralList[i][0] <= flooredNum)
+            {
 
-    if (flipArray[0] === '9') {
-        romanArray.unshift('IX');
-    }
-    if (flipArray[0] === '5') {
-        romanArray.unshift("V");
-    }
-
-    if((0 < parseInt(flipArray[0])) && (parseInt(flipArray[0]) < 4)) {
-        var insertedNum = parseInt(flipArray[0]);
-
-        while((5 < insertedNum) && (insertedNum < 9)) {
-            romanArray.unshift("I");
-            insertedNum--;
-
+                while(flooredNum > 0)
+                {
+                    romanArray.push(numeralList[i][1]);
+                    num = num - numeralList[i][0];
+                    flooredNum = flooredNum - numeralList[i][0];
+                }
+            }
         }
     }
-
-    if((5 < parseInt(flipArray[0])) && (parseInt(flipArray[0]) < 9)) {
-        var insertedNum = parseInt(flipArray[0]);
-
-        while((5 < insertedNum) && (insertedNum < 9)) {
-            romanArray.unshift("I");
-            insertedNum--;
-
-        }
-        romanArray.unshift('V');
-
-    }
-    //
-    //
-    // /********** Tens Place *********************************/
-    // if(flipArray[1] === '4'){
-    //     romanArray.unshift('XL');
-    //
-    // }
-    // if (flipArray[1] === '9'){
-    //     romanArray.unshift('XC');
-    //
-    // }
-    // if (flipArray[1] === '5'){
-    //     romanArray.unshift("L");
-    // }
-    //
-    // /********** Hundreds Place *****************************/
-    //
-    // if(flipArray[2] === '4'){
-    //     romanArray.unshift('CD');
-    //
-    // } else if (flipArray[2] === '9'){
-    //     romanArray.unshift('CM');
-    //
-    // }
-    //
-    // /********* Thousand **********************************/
-
     var romanNumeral = romanArray.join('');
     return romanNumeral;
-};
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,29 @@
 describe('romanConvert', function() {
-  it("if user input is higher then 3999 send error", function(){
-    expect(romanConvert(4000)).to.equal("Roman numerals can not go higher then 3,999 :(");
+  // it("if user input is higher then 3999 send error", function(){
+  //   expect(romanConvert(4000)).to.equal("Roman numerals can not go higher then 3,999 :(");
+  // });
+  //
+  it("Convert number to a roman numeral", function(){
+    expect(romanConvert(3)).to.equal('III');
   });
 
-  it("Convert number to an array of strings", function(){
+  it("Convert number to a roman numeral", function(){
     expect(romanConvert(8)).to.equal('VIII');
+  });
+
+  it("Convert number to a roman numeral", function(){
+    expect(romanConvert(53)).to.equal('LIII');
+  });
+
+  it("Convert number to a roman numeral", function(){
+    expect(romanConvert(68)).to.equal('LXVIII');
+  });
+
+  it("Convert number to a roman numeral", function(){
+    expect(romanConvert(550)).to.equal('DL');
+  });
+
+  it("Convert number to a roman numeral", function(){
+    expect(romanConvert(3450)).to.equal('MMMCDL');
   });
 });
